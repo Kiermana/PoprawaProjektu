6350b0fbcb498dc09838898ed9c76dac7187dd87
Kierman <jkiresztura8@gmail.com>
2025-09-07 11:51:23 +0200

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -19,27 +19,27 @@ public class Menu {
             System.out.println("4: usun gre.");
             System.out.println("5: wyjscie");
 
-            String wybor = scanner.nextInt();
+            String wybor = scanner.nextLine();
 
             try {
                 switch (wybor) {
-                    case 1 :
+                    case "1" :
                             dodajGre();
                             break;
 
-                case 2 :
+                case "2" :
                         pokazGry();
                         break;
 
-                    case 3 :
+                    case "3" :
                         ZmienCene();
                         break;
 
-                    case 4:
+                    case "4":
                         UsunGre();
                         break;
 
-                    case 5 :
+                    case "5" :
                         menu = false;
                         break;
 
@@ -53,6 +53,60 @@ public class Menu {
         }
     }
     private void dodajGre() {
-        System.out.println("Podaj id: ");
+        System.out.println("Tytul: ");
+        String tytul = scanner.nextLine();
+
+        System.out.println("Producent: ");
+        String developers = scanner.nextLine();
+
+        System.out.println("Cena: ");
+        double cena = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Podaj gatunki (oddziel je znakiem ,): ");
+        String[] gatunki = scanner.nextLine().split(",");
+
+        System.out.println("Rodzaj gry (C = cyfrowa, P = Pudelkowa");
+        String rodzaj = scanner.nextLine().toUpperCase();
+
+        if(rodzaj.equals("C")) {
+            System.out.println("Podaj platforme (np: steam, epic games, gog");
+            String platforma = scanner.nextLine();
+            biblioteka.dodaj(new Cyfrowa(tytul, developers, cena, gatunki, platforma));
+        }
+        else if (rodzaj.equals("P")){
+            System.out.println("Podaj Edycje (np Podstawowa, Deluxe, Ultimate");
+            String edycja = scanner.nextLine();
+            biblioteka.dodaj(new Pudelkowa(tytul, developers, cena, gatunki, edycja));
+        }
+        else {
+            System.out.println("Zostal podany zly typ gry");
+        }
+        System.out.println("Gra zostala dodana");
+    }
+
+    private void pokazGry(){
+        if (biblioteka.getAll().isEmpty()){
+            System.out.println("Brak gier w bibliotece");
+        }
+        else {
+            for (Gra y : biblioteka.getAll()){
+                System.out.println(y);
+            }
+        }
+    }
+
+    private void ZmienCene() {
+        System.out.println("Podaj ID gry: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Nowa cena: ");
+        double cena = Double.parseDouble(scanner.nextLine());
+        biblioteka.UpdateCena(id, cena);
+        System.out.println("Cena gry zostala zmieniona");
+    }
+    private void UsunGre(){
+        System.out.println("Podaj ID gry: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        biblioteka.usun(id);
+        System.out.println("Gra zostala usunieta z systemu.");
     }
 }
