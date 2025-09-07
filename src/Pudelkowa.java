65eb024a1a3fe1a07e3389112ce17c19dd146e07
Kierman <jkiresztura8@gmail.com>
2025-09-07 11:51:05 +0200

    Update Pudelkowa.java

--- src/Pudelkowa.java
+++ src/Pudelkowa.java
@@ -1,8 +1,8 @@
 public class Pudelkowa extends Gra{
     private String edycja;
 
-    public Pudelkowa(int id, String tytul, String developer, double cena, String[] gatunki, String edycja) {
-        super(id, tytul, developer, cena, gatunki);
+    public Pudelkowa(String tytul, String developer, double cena, String[] gatunki, String edycja) {
+        super(tytul, developer, cena, gatunki);
         this.edycja = edycja;
     }
 
