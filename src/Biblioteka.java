8ea8e68deab5201a53d95d83324a00d75ef117e5
Kierman <jkiresztura8@gmail.com>
2025-09-08 02:17:46 +0200

    Update Biblioteka.java

--- src/Biblioteka.java
+++ src/Biblioteka.java
@@ -2,23 +2,24 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Biblioteka {
+    //wykorzystanie Arraylist do przechowywania listy gier
     private ArrayList<Gra> gras = new ArrayList<>();
 
-
+    //Metoda dodająca grę do listy
     public void dodaj(Gra gra){
         gras.add(gra);
     }
-
+    //Metoda wypisująca wszystkie gry
     public ArrayList<Gra> getAll() {
         return gras;
     }
-
     public Gra findbyid(int id){
         for (Gra x : gras) {
             if (x.getId() == id) return x;
         }
         return null;
     }
+    //Metoda aktualizująca cenę gry
     public void UpdateCena(int id, double NowaCena){
         Gra y = findbyid(id);
         if(y != null) {
@@ -28,6 +29,7 @@ public class Biblioteka {
             throw new IllegalArgumentException("Nie znaleziono gry o podanym ID.");
         }
     }
+    //Metoda usuwająca grę z listy
     public void usun(int id){
         Gra y = findbyid(id);
         if(y != null){
