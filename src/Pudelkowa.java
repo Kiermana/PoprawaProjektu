875ef68391c419389a26edbf17abbb53b9ed2ffe
Kierman <jkiresztura8@gmail.com>
2025-09-08 05:02:24 +0200

    Update Pudelkowa.java

--- src/Pudelkowa.java
+++ src/Pudelkowa.java
@@ -8,9 +8,14 @@ public class Pudelkowa extends Gra{
 
     //Tak samo jak w klasie cyfrowa, została użyta komenda super.toString aby kod został zoptymalizowany i zamiast pisać od nowa cały toString zostaje wykorzystany on z klasy Gra i są dodane do niego tylko informacje o edycji gry
     @Override
-    public String toString() {
-        return super.toString() + "Pudelkowa{" +
-                "edycja=' " + edycja + '\'' +
+   public String toString() {
+        return "Pudelkowa{" +
+                "id=" + getId() +
+                ", tytul='" + getTytul() + '\'' +
+                ", developer='" + getDeveloper() + '\'' +
+                ", cena=" + getCena() +
+                ", gatunki=" + Arrays.toString(getGatunki()) +
+                ", edycja='" + edycja + '\'' +
                 '}';
     }
 }
